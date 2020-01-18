c4bff490c1467fb9bc02f47e4a5c19759d452ae4
dai <dai@tagbangers.co.jp>
2020-01-18 17:14:37 +0900

    implement Part03

--- src/main/java/io/pivotal/literx/Part03StepVerifier.java
+++ src/main/java/io/pivotal/literx/Part03StepVerifier.java
@@ -16,6 +16,7 @@
 
 package io.pivotal.literx;
 
+import java.time.Duration;
 import java.util.function.Supplier;
 
 import io.pivotal.literx.domain.User;
@@ -44,7 +45,9 @@ public class Part03StepVerifier {
 
 	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then a RuntimeException error.
 	void expectFooBarError(Flux<String> flux) {
-		fail();
+		StepVerifier
+			.create(flux)
+			.expectError(RuntimeException.class);
 	}
 
 //========================================================================================
@@ -52,14 +55,21 @@ public class Part03StepVerifier {
 	// TODO Use StepVerifier to check that the flux parameter emits a User with "swhite"username
 	// and another one with "jpinkman" then completes successfully.
 	void expectSkylerJesseComplete(Flux<User> flux) {
-		fail();
+		StepVerifier
+			.create(flux)
+			.expectNextMatches(u -> "swhite".equals(u.getUsername()))
+			.expectNextMatches(u -> "jpinkman".equals(u.getUsername()))
+			.verifyComplete()	;
 	}
 
 //========================================================================================
 
 	// TODO Expect 10 elements then complete and notice how long the test takes.
 	void expect10Elements(Flux<Long> flux) {
-		fail();
+		StepVerifier
+			.create(flux)
+			.expectNextCount(10)
+			.verifyComplete();
 	}
 
 //========================================================================================
@@ -67,7 +77,12 @@ public class Part03StepVerifier {
 	// TODO Expect 3600 elements at intervals of 1 second, and verify quicker than 3600s
 	// by manipulating virtual time thanks to StepVerifier#withVirtualTime, notice how long the test takes
 	void expect3600Elements(Supplier<Flux<Long>> supplier) {
-		fail();
+		StepVerifier
+				.withVirtualTime(supplier)
+				.thenAwait(Duration.ofHours(1))
+				.expectNextCount(3600)
+				.verifyComplete();
+
 	}
 
 	private void fail() {
